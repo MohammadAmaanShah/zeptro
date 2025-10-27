2fe9310bffcf5b313e530239afc134646715ebec
MohammadAmaanShah <mamaanshah2004@gmail.com>
2025-10-28 00:41:48 +0530

    final  dep

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Carousel from '../components/carousel'
+import Carousel from '../components/Carousel'
 import Category from '../components/Category'
 import MidBanner from '../components/MidBanner'
 import Feature from '../components/Feature'
